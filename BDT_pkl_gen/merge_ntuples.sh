2cab374f7116a7ab0fbf4e0099406644a4fbfa6d
Joe <joebob.bateman@gmail.com>
2025-06-10 08:15:32 -0500

    Script for staging and hadd-ing sam definitions

---
+++ BDT_pkl_gen/merge_ntuples.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# This script gets the file paths associated with a samdef, and hadds them into a single file.
+
+SAMDEF=$1
+
+# Clear the output file if it exists and rewrite the file names to it
+> temp_filelist.txt
+samweb list-files "defname:$SAMDEF" > temp_filelist.txt
+
+# # Check if the sample is prestaged
+# samweb prestage-dataset --defname=$SAMDEF
+
+# Go through temp_filelist and get the path of each file
+> filelist.list
+while read -r line; do
+    # Check if the line is not empty
+    FILEPATH=$(samweb locate-file $line)
+    # Remove leading enstore: from the file path
+    FILEPATH=$(echo "$FILEPATH" | sed 's/^enstore://')
+    FILEPATH=$(echo "$FILEPATH" | sed 's/([^)]*)//g')
+    if [[ -n "$line" ]]; then
+        echo "$FILEPATH/$line" >> filelist.list
+    fi
+done < temp_filelist.txt
+
+hadd /exp/uboone/data/users/jbateman/workdir/HPS_uboone_analysis/Run_245_ana/"$SAMDEF.root" @filelist.list
